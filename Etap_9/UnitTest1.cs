e1f4129e8efb3d0a8ad51bfad6c1b03a2ae6f4ab
who? <forhwfromuniversity@gmail.com>
2023-09-30 21:44:20 +0300

    etap_9

---
+++ Etap_9/UnitTest1.cs
@@ -0,0 +1,42 @@
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+
+namespace Etap_9
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private IWebDriver _driver;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _driver = new EdgeDriver();
+        }
+
+        [TestMethod]
+        public void UserCanLogin()
+        {
+            _driver.Navigate().GoToUrl("https://localhost:44335/Genres/Create");
+
+            var nameInput = _driver.FindElement(By.Id("Name"));
+            var submitInput = _driver.FindElement(By.Id("SubmitButton"));
+
+            nameInput.SendKeys("test");
+            submitInput.Click();
+
+            _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+            var navbar = _driver.FindElement(By.Id("linkTest"));
+            Assert.IsNotNull(navbar);
+
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+            _driver.Quit();
+        }
+    }
+}
